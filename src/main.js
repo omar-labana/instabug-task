0ed6f14a237ca42bf8d89015215794c1f0e2acbf
Omar Labana <omarlabana@outlook.com>
2021-06-02 19:54:50 +0200

    Import sass styles globaly

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 
 import SVG from "./components/SVG.vue"
+import './assets/sass/_atomic.scss';
 
 const app = createApp(App)
 app.use(router)
