d3b516edfaf5e0691014249265126543f7033c5d
Omar Labana <omarlabana@outlook.com>
2021-06-02 19:34:28 +0200

    Delete Dummy component and configure router view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Home msg="Hello Vue 3 + Vite" />
+  <router-view></router-view>
 </template>
 
 <script setup>
-import Home from './components/Home.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
