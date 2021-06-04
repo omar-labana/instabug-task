977fd28690365685db4cf8c154c5971126f7198a
Omar Labana <omarlabana@outlook.com>
2021-06-04 19:29:26 +0200

    Finish login/logout logic

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,33 @@
 <template>
     <main class="full-page flex flex-col content-center">
-        <h1>Welcome, Omar</h1>
-        <button type="button" class="mt-10 input-padding input-border text-white bg-blue">
+        <h1>Welcome, {{ email }}</h1>
+        <button
+            @click="logout"
+            type="button"
+            class="mt-10 input-padding input-border text-white bg-blue"
+        >
             Log out
         </button>
     </main>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            email: '',
+        }
+    },
+    mounted() {
+        this.email = JSON.parse(localStorage.getItem('user')).email
+    },
+    methods: {
+        logout: function () {
+            localStorage.removeItem('user')
+            this.$router.push('/')
+        },
+    },
+}
 </script>
 
 <style>
