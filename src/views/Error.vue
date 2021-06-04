754a8fe0d31b98a5609ce221db5b1e490c14fe27
Omar Labana <omarlabana@outlook.com>
2021-06-04 10:29:34 +0200

    Add 404 page layout

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1>error</h1>
+    <main class="full-page flex flex-col content-center">
+        <img src="../assets/404.png" alt="404 image" />
+        <h2 class="text-blue-dark">404 page not found</h2>
+        <h3 class="label-text">Sorry, that page doesn't exit,</h3>
+    </main>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
