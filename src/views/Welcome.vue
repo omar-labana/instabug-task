7d92874ee9b3ff51e5006038738313291c3192e5
Omar Labana <omarlabana@outlook.com>
2021-06-04 10:29:51 +0200

    Add welcome page layout

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1>welcome</h1>
+    <main class="full-page flex flex-col content-center">
+        <h1>Welcome, Omar</h1>
+        <button type="button" class="mt-10 input-padding input-border text-white bg-blue">
+            Log out
+        </button>
+    </main>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
